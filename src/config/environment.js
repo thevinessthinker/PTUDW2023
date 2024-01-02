4327eb5329ade137e6cc3f47bc9ca0ce17159b0a
1760241 <luuthienvi.github@gmail.com>
2024-01-02 15:15:18 +0700

    [#1.1] Add db config.

---
+++ src/config/environment.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const env = {
+    SERVER_HOSTNAME: process.env.SERVER_HOSTNAME,
+    SERVER_PORT: process.env.SERVER_PORT,
+    DB_HOSTNAME: process.env.DB_HOSTNAME,
+    DB_PORT: process.env.DB_PORT,
+    DB_NAME: process.env.DB_NAME,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+};
+
+module.exports = env;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,45 @@
 const express = require('express');
-const app = express();
+const env = require('./config/environment');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const { CONNECT_DB } = require('./config/db');
 
-app.get('/', (req, res) => {
-    res.send('Hello World, OK');
-});
+const START_SERVER = () => {
+    const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is listening at http://localhost:3000');
-});
+    require('./config/view')(app);
+    app.use('/static', express.static('src/public'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.get('/', (req, res) => {
+        res.send('Hello world');
+    });
+
+    const server = https.createServer(
+        {
+            key: fs.readFileSync(path.join(__dirname, '../_certs/server.key')),
+            cert: fs.readFileSync(
+                path.join(__dirname, '../_certs/server.cert'),
+            ),
+        },
+        app,
+    );
+    server.listen(env.SERVER_PORT, env.SERVER_HOSTNAME, () => {
+        console.log(
+            `3. [OK] Server is running at https://${env.SERVER_HOSTNAME}:${env.SERVER_PORT} `,
+        );
+    });
+};
+
+/////////////////////////////////////
+(async () => {
+    try {
+        console.log('\n1. [...] Connecting to PostgreSQL database');
+        CONNECT_DB();
+        console.log('2. [OK] Connected to PostgreSQL database');
+        START_SERVER();
+    } catch (err) {
+        console.error('ERROR: ' + err);
+    }
+})();
