de2b98ce67dcfa212a4c87e55a46b0402b1bac08
1760445 <102032780+K1HaRu@users.noreply.github.com>
2024-01-09 16:03:01 +0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,2 +1,6 @@
---test
+--insert data 
 insert into categories (category_name) values ('Thời trang nữ');
+insert into categories (category_name) values ('Thời trang nam');
+insert into categories (category_name) values ('Đồng hồ');
+insert into categories (category_name) values ('Điện thoại & Phụ kiện');
+insert into categories (category_name) values ('Máy tính & Laptop');
