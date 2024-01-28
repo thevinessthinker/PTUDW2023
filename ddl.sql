fe7d14ed11fdbea62ac57e4dee526d42dffc6537
K1HaRu <nguyenthibichtram371@gmail.com>
2024-01-28 17:20:08 +0700

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -134,3 +134,33 @@ CREATE TABLE IF NOT EXISTS verification_tokens (
 );
 
 -- END
+-- ----------------------------
+-- Table structure for Categories
+-- ----------------------------
+BEGIN;
+insert into categories (category_name) values ('Thời trang nữ');
+insert into categories (category_name) values ('Thời trang nam');
+insert into categories (category_name) values ('Đồng hồ');
+insert into categories (category_name) values ('Điện thoại & Phụ kiện');
+insert into categories (category_name) values ('Máy tính & Laptop');
+insert into categories (category_name) values ('Sách');
+COMMIT;
+-- ----------------------------
+-- Table structure for product_types
+-- ----------------------------
+BEGIN;
+insert into product_types (type_name) values ('Áo thun');
+insert into product_types (type_name) values ('Áo khoác');
+insert into product_types (type_name) values ('Áo sơ mi');
+insert into product_types (type_name) values ('Quần dài');
+insert into product_types (type_name) values ('Quần jean');
+insert into product_types (type_name) values ('Quần đùi');
+insert into product_types (type_name) values ('Vớ/Tất');
+
+insert into product_types (type_name) values ('Chân váy');
+insert into product_types (type_name) values ('Đầm/Váy');
+insert into product_types (type_name) values ('Áo len & Cardigan');
+insert into product_types (type_name) values ('Đồ truyền thống');
+
+insert into product_types (type_name) values ('Khác');
+COMMIT;
